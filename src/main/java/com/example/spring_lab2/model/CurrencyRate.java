db15098f0c59e80ad087011cbd5efa529c4feff6
Shapovalov Andrii <141683231+nonGratis@users.noreply.github.com>
2024-12-24 23:38:30 +0200

    refactor by replace currencyName with Currency entity and delete necessary metadata

--- src/main/java/com/example/spring_lab2/model/CurrencyRate.java
+++ src/main/java/com/example/spring_lab2/model/CurrencyRate.java
@@ -11,8 +11,9 @@ public class CurrencyRate {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "currency_name", nullable = false)
-    private String currencyName;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "currency_id", nullable = false)
+    private Currency currency;
 
     @Column(name = "rate_date", nullable = false)
     private LocalDate date;
@@ -20,39 +21,25 @@ public class CurrencyRate {
     @Column(name = "exchange_rate", nullable = false)
     private double exchangeRate;
 
-    @Column(name = "previous_rate")
-    private double previousRate;
-
-    @Column(name = "rate_change")
-    private double rateChange;
-
-
     public CurrencyRate() {
     }
 
-
-    public CurrencyRate(String currencyName, LocalDate date, double exchangeRate, double previousRate, double rateChange) {
-        this.currencyName = currencyName;
+    public CurrencyRate(Currency currency, LocalDate date, double exchangeRate) {
+        this.currency = currency;
         this.date = date;
         this.exchangeRate = exchangeRate;
-        this.previousRate = previousRate;
-        this.rateChange = rateChange;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Currency getCurrency() {
+        return currency;
     }
 
-    public String getCurrencyName() {
-        return currencyName;
-    }
-
-    public void setCurrencyName(String currencyName) {
-        this.currencyName = currencyName;
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
     }
 
     public LocalDate getDate() {
@@ -69,21 +56,5 @@ public class CurrencyRate {
 
     public void setExchangeRate(double exchangeRate) {
         this.exchangeRate = exchangeRate;
-    }
-
-    public double getPreviousRate() {
-        return previousRate;
-    }
-
-    public void setPreviousRate(double previousRate) {
-        this.previousRate = previousRate;
-    }
-
-    public double getRateChange() {
-        return rateChange;
-    }
-
-    public void setRateChange(double rateChange) {
-        this.rateChange = rateChange;
     }
 }
